036aa1b642ab07f8a89ce53203bbdf6f60b7caef
Alec Nazworth <nazworthalec@outlook.com>
2024-03-03 19:40:45 -0500

    -recommiting to merge master

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,9 +3,9 @@ function Profile() {
     <div className="mt-40 mb-40 text-sky-300 flex flex-col flex-wrap justify-center align-center items-center">
       <h1 className="text-5xl text-gray-800">Alec Nazworth</h1>
       <p className="mx-auto w-3/6 text-center mt-5 bg-gray-800 p-7 rounded-2xl">
-        I'm a CS student at Florida International University and Combat Medic in
-        the Army National Guard. I have a deep interest in web development. I am
-        currently learning to build complex web applications using React and
+        I am a CS student at Florida International University and Combat Medic
+        in the Army National Guard. I have a deep interest in web development. I
+        am currently learning to build complex web applications using React and
         other tools.
       </p>
     </div>
